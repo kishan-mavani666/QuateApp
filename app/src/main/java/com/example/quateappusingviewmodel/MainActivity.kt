fade7d04f985b45b718e3adaa085f4f7f9ffdc7b
Kishan Mavani <mavanikishan666@gmail.com>
2023-03-01 00:57:07 +0530

    -Initial commit.
    -Initial commit.

---
+++ app/src/main/java/com/example/quateappusingviewmodel/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.quateappusingviewmodel
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.example.quateappusingviewmodel.model.Quote
+import com.example.quateappusingviewmodel.viewmodel.MainViewModel
+import com.example.quateappusingviewmodel.viewmodel.MainViewModelFactory
+
+class MainActivity : AppCompatActivity() {
+    lateinit var mainViewModel: MainViewModel
+
+    private val quoteText: TextView
+        get() = findViewById(R.id.quoteText)
+
+    private val quoteAuthor: TextView
+        get() = findViewById(R.id.quoteAuthor)
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mainViewModel = ViewModelProvider(
+            this,
+            MainViewModelFactory(application)
+        )[MainViewModel::class.java]
+        setQuote(mainViewModel.getQuote())
+
+    }
+
+    private fun setQuote(quote: Quote) {
+        quoteText.text = quote.text
+        quoteAuthor.text = quote.author
+    }
+
+    fun onPrevious(view: View) {
+        setQuote(mainViewModel.previousQuote())
+    }
+
+    fun onNext(view: View) {
+        setQuote(mainViewModel.nextQuote())
+    }
+
+    fun onShare(view: View) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_TEXT, mainViewModel.getQuote().text)
+        startActivity(intent)
+    }
+
+
+}
